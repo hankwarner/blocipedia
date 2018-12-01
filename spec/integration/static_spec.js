e39a41ac64203f769259fec5ee94ca27ec9b2ca1
hankwarner <40283259+hankwarner@users.noreply.github.com>
2018-11-30 22:54:34 -0500

    scaffolding checkpoint

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -5,10 +5,10 @@ const base = "http://localhost:3000/";
 describe("routes : static", () => {
 
   describe("GET /", () => {
-    it("should return status code 200 and have 'Welcome to Blocipedia' in the body of the response", () => {
+    it("should return status code 200 and have 'Blocipedia' in the body of the response", () => {
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
-        expect(body).toContain("Welcome to Blocipedia");
+        expect(body).toContain("Blocipedia");
         done();
       })
     })
--- src/config/main-config.js
+++ src/config/main-config.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const path = require("path");
 const viewsFolder = path.join(__dirname, "..", "views");
 const bodyParser = require("body-parser");
+const logger = require('morgan');
 
 module.exports = {
   init(app, express){
@@ -9,5 +10,6 @@ module.exports = {
     app.set("view engine", "ejs");
     app.use(express.static(path.join(__dirname, "..", "assets")));
     app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(logger('dev'));
   }
 };

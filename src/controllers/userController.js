5b6f109772d13b45591c0c7be340b771736ba573
hankwarner <40283259+hankwarner@users.noreply.github.com>
2018-12-03 15:27:57 -0500

    working through SendGrid

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,6 @@
 const userQueries = require("../db/queries.users.js");
 const passport = require("passport");
+const sgMail = require('@sendgrid/mail');
 
 module.exports = {
     signUp(req, res, next){
@@ -12,20 +13,30 @@ module.exports = {
  
     create(req, res, next){
         let newUser = {
-        email: req.body.email,
-        username: req.body.username,
-        password: req.body.password,
-        passwordConfirmation: req.body.passwordConfirmation
+            email: req.body.email,
+            username: req.body.username,
+            password: req.body.password,
+            passwordConfirmation: req.body.passwordConfirmation
         };
 
         userQueries.createUser(newUser, (err, user) => {
+            const msg = {
+                to: req.body.email,
+                from: 'noreply@blocipedia.com',
+                subject: 'Thanks for joining Blocipedia',
+                text: 'Thanks for registering for your free account with Blocipedia!',
+                html: '<strong>Thanks for registering for your free account with Blocipedia!</strong>',
+              };
+
             if(err){
                 req.flash("error", err);
-                res.redirect("/users/sign_up");
+                res.redirect("/users/signup");
             } else {
                 passport.authenticate("local")(req, res, () => {
-                req.flash("notice", "You've successfully signed in!");
-                res.redirect("/");
+                    req.flash("notice", "You've successfully signed in!");
+                    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+                    sgMail.send(msg);
+                    res.redirect("/");
                 })
             }
         })

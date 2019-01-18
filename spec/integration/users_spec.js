69516740c7231586af21cbde8de22f152b35245e
hankwarner <40283259+hankwarner@users.noreply.github.com>
2019-01-17 23:30:47 -0500

    added specs to users_spec

--- spec/integration/users_spec.js
+++ spec/integration/users_spec.js
@@ -26,6 +26,56 @@ describe("routes : users", () => {
     })
   })
 
+  describe("POST /users", () => {
+    it("should create a new user with valid values and redirect", (done) => {
+      const options = {
+        url: base,
+        form: {
+          email: 'megaman@capcom.com',
+          username: 'megaman',
+          password: 'pewpewpew',
+        }
+      }
+
+      request.post(options, (err, res, body) => {
+        User.findOne({where: {email: 'megaman@capcom.com'}})
+        .then((user) => {
+          expect(user).not.toBeNull();
+          expect(user.email).toBe('megaman@capcom.com');
+          expect(user.id).toBe(1);
+          done();
+        })
+        .catch((err) => {
+          console.log(err);
+          done();
+        })
+      })
+    })
+
+    it("should not create a new user with invalid attributes and redirect", (done) => {
+      const options = {
+        url: base,
+        form: {
+          email: 'megaman@',
+          username: 'megaman',
+          password: 'pewpewpew',
+        }
+      }
+      
+      request.post(options, (err, res, body) => {
+        User.findOne({where: {username: 'megaman'}})
+        .then((user) => {
+          expect(user).toBeNull();
+          done();
+        })
+        .catch((err) => {
+          console.log(err);
+          done();
+        })
+      })
+    })
+  })
+
   describe("GET /users/signin", () => {
     it("should render a view with a sign in form", (done) => {
       request.get(`${base}signin`, (err, res, body) => {

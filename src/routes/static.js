e95b29e7b0167b4a22657c0d55074cd87e078166
hankwarner <40283259+hankwarner@users.noreply.github.com>
2019-01-17 23:45:40 -0500

    testing cron job

--- src/routes/static.js
+++ src/routes/static.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const CronJob = require('../lib/cron.js').CronJob;
 const staticController = require("../controllers/staticController");
 
 router.get("/", staticController.index);
+
+const job = new CronJob('0 */10 * * * *', () => {
+    router.get(router.get("/", staticController.index));
+    console.log('ping');
+});
+
+job.start();
 
 module.exports = router;

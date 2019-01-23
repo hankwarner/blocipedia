ccc7c14cdaafc51c6b9c25c8d52f246f037750f6
hankwarner <40283259+hankwarner@users.noreply.github.com>
2019-01-22 21:18:07 -0500

    working on favicon

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
+const favicon = require('express-favicon');
 
 const appConfig = require("./config/main-config.js");
 const routeConfig = require("./config/route-config.js");
 
 appConfig.init(app, express);
+app.use(express.favicon(__dirname + '/public/favicon.ico'));
+
 routeConfig.init(app);
 
 module.exports = app;
